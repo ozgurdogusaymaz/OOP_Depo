6eb6c241ee106846f45386148d30a79506c34c55
ozgurdogusaymaz <ozgurdogusaymaz@hotmail.com>
2016-12-18 18:45:32 +0300

    YazıHane

---
+++ YazıhaneApp/YazıhaneApp/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace YazıhaneApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtCinsiyet = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtOtobusTuru = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(47, 62);
+            this.TxtAd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(148, 22);
+            this.TxtAd.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad Soyad";
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(47, 309);
+            this.BtnKaydet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(148, 28);
+            this.BtnKaydet.TabIndex = 2;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(44, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cinsiyet";
+            // 
+            // TxtCinsiyet
+            // 
+            this.TxtCinsiyet.Location = new System.Drawing.Point(47, 153);
+            this.TxtCinsiyet.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtCinsiyet.Name = "TxtCinsiyet";
+            this.TxtCinsiyet.Size = new System.Drawing.Size(148, 22);
+            this.TxtCinsiyet.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 209);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Otobus Türü";
+            // 
+            // TxtOtobusTuru
+            // 
+            this.TxtOtobusTuru.Location = new System.Drawing.Point(47, 238);
+            this.TxtOtobusTuru.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtOtobusTuru.Name = "TxtOtobusTuru";
+            this.TxtOtobusTuru.Size = new System.Drawing.Size(148, 22);
+            this.TxtOtobusTuru.TabIndex = 5;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(218, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(363, 228);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 392);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtOtobusTuru);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtCinsiyet);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtAd);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtCinsiyet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtOtobusTuru;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
