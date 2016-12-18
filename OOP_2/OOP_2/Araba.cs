0a46e880f2daac4e848c85d3300dca8b5f0b8582
ozgurdogusaymaz <ozgurdogusaymaz@hotmail.com>
2016-12-18 18:15:16 +0300

    OOP_2

---
+++ OOP_2/OOP_2/Araba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_2
+{
+    
+    class Araba
+    {
+        int _motorgucu;
+
+        public int MotorGucu
+        {
+            get
+            {
+                return _motorgucu;
+            }
+            set
+            {
+                if (value > 800 && value < 5000)
+                {
+                    _motorgucu = value;
+                }
+            }
+        }
+    }
+}
